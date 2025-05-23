36f45f454f8c3298fbe77485177866a4efc5e885
Hannah Fisher <hannahtfisher@gmail.com>
2025-05-23 22:37:59 +0100

    Set up player sprite and animation (and moved assets into more organised folder)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float m_moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D m_rb;
+    [SerializeField] private Animator m_animator;
+    private Vector2 m_moveInput;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rb.linearVelocity = m_moveInput * m_moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        m_animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            m_animator.SetBool("isWalking", false);
+            m_animator.SetFloat("LastInputX", m_moveInput.x);
+            m_animator.SetFloat("LastInputY", m_moveInput.y);
+        }
+        
+        m_moveInput = context.ReadValue<Vector2>();
+        m_animator.SetFloat("InputX", m_moveInput.x);
+        m_animator.SetFloat("InputY", m_moveInput.y);
+        
+    }
+}
